81abf83f87331ffb99e73c4ec27453664cab275a
Michael Gieson <mgieson@plaino.com>
2025-01-27 20:14:58 -0500

    update and build

--- CHANGE LOG.md
+++ CHANGE LOG.md
@@ -2,3 +2,4 @@
 - moved "remember" to use a single json key
 - set localstorage key to the URL -- so other layoutSplitters don't interfere with eachother.
 - swapped parseFloat to custom toNumber
+- set example.html to use remember
---
+++ dist/build/docs/_menuData.js
@@ -0,0 +1,360 @@
+var MenuData = [
+	{
+		"id": "root-packages",
+		"url": "root.html#packages",
+		"label": "packages",
+		"kind": "packages",
+		"children": [
+			{
+				"id": "jbeeb.utils",
+				"url": "jbeeb.utils.html#jbeeb.utils",
+				"label": "jbeeb.utils",
+				"kind": "packages-item",
+				"access": "public",
+				"inherits": 0,
+				"children": [
+					{
+						"id": "jbeeb.utils-classes",
+						"url": "jbeeb.utils.html#classes",
+						"label": "classes",
+						"kind": "classes",
+						"children": [
+							{
+								"id": "jbeeb.utils.Layout",
+								"url": "jbeeb.utils.Layout.html#jbeeb.utils.Layout",
+								"label": "Layout",
+								"kind": "classes-item",
+								"access": "public",
+								"inherits": 0,
+								"children": [
+									{
+										"id": "jbeeb.utils.Layout-events",
+										"url": "jbeeb.utils.Layout.html#events",
+										"label": "events",
+										"kind": "events",
+										"children": [
+											{
+												"id": "jbeeb.utils.Layout.globalResize",
+												"url": "jbeeb.utils.Layout.html#jbeeb.utils.Layout.globalResize",
+												"label": "globalResize",
+												"kind": "events-item",
+												"access": "public",
+												"inherits": 0
+											},
+											{
+												"id": "jbeeb.utils.Layout.globalResizeComplete",
+												"url": "jbeeb.utils.Layout.html#jbeeb.utils.Layout.globalResizeComplete",
+												"label": "globalResizeComplete",
+												"kind": "events-item",
+												"access": "public",
+												"inherits": 0
+											},
+											{
+												"id": "jbeeb.utils.Layout.handleOnResize",
+												"url": "jbeeb.utils.Layout.html#jbeeb.utils.Layout.handleOnResize",
+												"label": "handleOnResize",
+												"kind": "events-item",
+												"access": "private",
+												"inherits": 0
+											},
+											{
+												"id": "jbeeb.utils.Layout.handleOnResizeComplete",
+												"url": "jbeeb.utils.Layout.html#jbeeb.utils.Layout.handleOnResizeComplete",
+												"label": "handleOnResizeComplete",
+												"kind": "events-item",
+												"access": "private",
+												"inherits": 0
+											}
+										]
+									},
+									{
+										"id": "jbeeb.utils.Layout-methods",
+										"url": "jbeeb.utils.Layout.html#methods",
+										"label": "methods",
+										"kind": "methods",
+										"children": [
+											{
+												"id": "jbeeb.utils.Layout.addDomElemListenerWithArgs",
+												"url": "jbeeb.utils.Layout.html#jbeeb.utils.Layout.addDomElemListenerWithArgs",
+												"label": "addDomElemListenerWithArgs",
+												"kind": "methods-item",
+												"access": "private",
+												"inherits": 0
+											},
+											{
+												"id": "jbeeb.utils.Layout.dealWithPadding",
+												"url": "jbeeb.utils.Layout.html#jbeeb.utils.Layout.dealWithPadding",
+												"label": "dealWithPadding",
+												"kind": "methods-item",
+												"access": "private",
+												"inherits": 0
+											},
+											{
+												"id": "jbeeb.utils.Layout.findObjFromElem",
+												"url": "jbeeb.utils.Layout.html#jbeeb.utils.Layout.findObjFromElem",
+												"label": "findObjFromElem",
+												"kind": "methods-item",
+												"access": "private",
+												"inherits": 0
+											},
+											{
+												"id": "jbeeb.utils.Layout.findSibs",
+												"url": "jbeeb.utils.Layout.html#jbeeb.utils.Layout.findSibs",
+												"label": "findSibs",
+												"kind": "methods-item",
+												"access": "private",
+												"inherits": 0
+											},
+											{
+												"id": "jbeeb.utils.Layout.finishMove",
+												"url": "jbeeb.utils.Layout.html#jbeeb.utils.Layout.finishMove",
+												"label": "finishMove",
+												"kind": "methods-item",
+												"access": "private",
+												"inherits": 0
+											},
+											{
+												"id": "jbeeb.utils.Layout.formatPanels",
+												"url": "jbeeb.utils.Layout.html#jbeeb.utils.Layout.formatPanels",
+												"label": "formatPanels",
+												"kind": "methods-item",
+												"access": "private",
+												"inherits": 0
+											},
+											{
+												"id": "jbeeb.utils.Layout.getScrollbarSize",
+												"url": "jbeeb.utils.Layout.html#jbeeb.utils.Layout.getScrollbarSize",
+												"label": "getScrollbarSize",
+												"kind": "methods-item",
+												"access": "private",
+												"inherits": 0
+											},
+											{
+												"id": "jbeeb.utils.Layout.handleOnResize",
+												"url": "jbeeb.utils.Layout.html#jbeeb.utils.Layout.handleOnResize",
+												"label": "handleOnResize",
+												"kind": "methods-item",
+												"access": "private",
+												"inherits": 0
+											},
+											{
+												"id": "jbeeb.utils.Layout.handleOnResizeComplete",
+												"url": "jbeeb.utils.Layout.html#jbeeb.utils.Layout.handleOnResizeComplete",
+												"label": "handleOnResizeComplete",
+												"kind": "methods-item",
+												"access": "private",
+												"inherits": 0
+											},
+											{
+												"id": "jbeeb.utils.Layout.handleScroll",
+												"url": "jbeeb.utils.Layout.html#jbeeb.utils.Layout.handleScroll",
+												"label": "handleScroll",
+												"kind": "methods-item",
+												"access": "private",
+												"inherits": 0
+											},
+											{
+												"id": "jbeeb.utils.Layout.init",
+												"url": "jbeeb.utils.Layout.html#jbeeb.utils.Layout.init",
+												"label": "init",
+												"kind": "methods-item",
+												"access": "private",
+												"inherits": 0
+											},
+											{
+												"id": "jbeeb.utils.Layout.mouse_down",
+												"url": "jbeeb.utils.Layout.html#jbeeb.utils.Layout.mouse_down",
+												"label": "mouse_down",
+												"kind": "methods-item",
+												"access": "private",
+												"inherits": 0
+											},
+											{
+												"id": "jbeeb.utils.Layout.mouse_move",
+												"url": "jbeeb.utils.Layout.html#jbeeb.utils.Layout.mouse_move",
+												"label": "mouse_move",
+												"kind": "methods-item",
+												"access": "private",
+												"inherits": 0
+											},
+											{
+												"id": "jbeeb.utils.Layout.mouse_up",
+												"url": "jbeeb.utils.Layout.html#jbeeb.utils.Layout.mouse_up",
+												"label": "mouse_up",
+												"kind": "methods-item",
+												"access": "private",
+												"inherits": 0
+											},
+											{
+												"id": "jbeeb.utils.Layout.moveTo",
+												"url": "jbeeb.utils.Layout.html#jbeeb.utils.Layout.moveTo",
+												"label": "moveTo",
+												"kind": "methods-item",
+												"access": "private",
+												"inherits": 0
+											},
+											{
+												"id": "jbeeb.utils.Layout.parseNumUnits",
+												"url": "jbeeb.utils.Layout.html#jbeeb.utils.Layout.parseNumUnits",
+												"label": "parseNumUnits",
+												"kind": "methods-item",
+												"access": "private",
+												"inherits": 0
+											},
+											{
+												"id": "jbeeb.utils.Layout.pauseEvent",
+												"url": "jbeeb.utils.Layout.html#jbeeb.utils.Layout.pauseEvent",
+												"label": "pauseEvent",
+												"kind": "methods-item",
+												"access": "private",
+												"inherits": 0
+											},
+											{
+												"id": "jbeeb.utils.Layout.positionPanels",
+												"url": "jbeeb.utils.Layout.html#jbeeb.utils.Layout.positionPanels",
+												"label": "positionPanels",
+												"kind": "methods-item",
+												"access": "private",
+												"inherits": 0
+											},
+											{
+												"id": "jbeeb.utils.Layout.prepRoots",
+												"url": "jbeeb.utils.Layout.html#jbeeb.utils.Layout.prepRoots",
+												"label": "prepRoots",
+												"kind": "methods-item",
+												"access": "private",
+												"inherits": 0
+											},
+											{
+												"id": "jbeeb.utils.Layout.prepSizes",
+												"url": "jbeeb.utils.Layout.html#jbeeb.utils.Layout.prepSizes",
+												"label": "prepSizes",
+												"kind": "methods-item",
+												"access": "private",
+												"inherits": 0
+											},
+											{
+												"id": "jbeeb.utils.Layout.remember",
+												"url": "jbeeb.utils.Layout.html#jbeeb.utils.Layout.remember",
+												"label": "remember",
+												"kind": "methods-item",
+												"access": "private",
+												"inherits": 0
+											},
+											{
+												"id": "jbeeb.utils.Layout.resize",
+												"url": "jbeeb.utils.Layout.html#jbeeb.utils.Layout.resize",
+												"label": "resize",
+												"kind": "methods-item",
+												"access": "private",
+												"inherits": 0
+											},
+											{
+												"id": "jbeeb.utils.Layout.resizeLast",
+												"url": "jbeeb.utils.Layout.html#jbeeb.utils.Layout.resizeLast",
+												"label": "resizeLast",
+												"kind": "methods-item",
+												"access": "private",
+												"inherits": 0
+											},
+											{
+												"id": "jbeeb.utils.Layout.resizePanel",
+												"url": "jbeeb.utils.Layout.html#jbeeb.utils.Layout.resizePanel",
+												"label": "resizePanel",
+												"kind": "methods-item",
+												"access": "private",
+												"inherits": 0
+											},
+											{
+												"id": "jbeeb.utils.Layout.restore",
+												"url": "jbeeb.utils.Layout.html#jbeeb.utils.Layout.restore",
+												"label": "restore",
+												"kind": "methods-item",
+												"access": "private",
+												"inherits": 0
+											},
+											{
+												"id": "jbeeb.utils.Layout.ripDoc",
+												"url": "jbeeb.utils.Layout.html#jbeeb.utils.Layout.ripDoc",
+												"label": "ripDoc",
+												"kind": "methods-item",
+												"access": "private",
+												"inherits": 0
+											},
+											{
+												"id": "jbeeb.utils.Layout.scrollPanel",
+												"url": "jbeeb.utils.Layout.html#jbeeb.utils.Layout.scrollPanel",
+												"label": "scrollPanel",
+												"kind": "methods-item",
+												"access": "private",
+												"inherits": 0
+											}
+										]
+									}
+								]
+							}
+						]
+					}
+				]
+			}
+		]
+	},
+	{
+		"id": "root-classes",
+		"url": "root.html#classes",
+		"label": "classes",
+		"kind": "classes",
+		"children": [
+			{
+				"id": "root.resizeThrottle",
+				"url": "root.resizeThrottle.html#root.resizeThrottle",
+				"label": "resizeThrottle",
+				"kind": "classes-item",
+				"access": "public",
+				"inherits": 0,
+				"children": [
+					{
+						"id": "root.resizeThrottle-methods",
+						"url": "root.resizeThrottle.html#methods",
+						"label": "methods",
+						"kind": "methods",
+						"children": [
+							{
+								"id": "root.resizeThrottle.add",
+								"url": "root.resizeThrottle.html#root.resizeThrottle.add",
+								"label": "add",
+								"kind": "methods-item",
+								"access": "private",
+								"inherits": 0
+							},
+							{
+								"id": "root.resizeThrottle.addCallback",
+								"url": "root.resizeThrottle.html#root.resizeThrottle.addCallback",
+								"label": "addCallback",
+								"kind": "methods-item",
+								"access": "private",
+								"inherits": 0
+							},
+							{
+								"id": "root.resizeThrottle.resize",
+								"url": "root.resizeThrottle.html#root.resizeThrottle.resize",
+								"label": "resize",
+								"kind": "methods-item",
+								"access": "private",
+								"inherits": 0
+							},
+							{
+								"id": "root.resizeThrottle.runCallbacks",
+								"url": "root.resizeThrottle.html#root.resizeThrottle.runCallbacks",
+								"label": "runCallbacks",
+								"kind": "methods-item",
+								"access": "private",
+								"inherits": 0
+							}
+						]
+					}
+				]
+			}
+		]
+	}
+]
